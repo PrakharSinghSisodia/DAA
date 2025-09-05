9db8b1e3c67b5c7085648495932d4499b55e426b
Prakhar Singh Sisodia <prakharsinghsisodia@gmail.com>
2025-09-05 12:44:56 +0530

    Create Q1.c

---
+++ Extra Lab Codes/Q1.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+// Function to rotate the matrix by 90 degrees clockwise
+void rotate90(int n, int matrix[n][n]) {
+    // Step 1: Transpose the matrix
+    for (int i = 0; i < n; i++) {
+        for (int j = i; j < n; j++) {
+            int temp = matrix[i][j];
+            matrix[i][j] = matrix[j][i];
+            matrix[j][i] = temp;
+        }
+    }
+
+    // Step 2: Reverse each row
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n / 2; j++) {
+            int temp = matrix[i][j];
+            matrix[i][j] = matrix[i][n - j - 1];
+            matrix[i][n - j - 1] = temp;
+        }
+    }
+}
+
+// Function to print the matrix
+void printMatrix(int n, int matrix[n][n]) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int n;
+    printf("Enter size of matrix (n x n): ");
+    scanf("%d", &n);
+
+    int matrix[n][n];
+
+    printf("Enter elements of the matrix:\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    printf("\nOriginal Matrix:\n");
+    printMatrix(n, matrix);
+
+    rotate90(n, matrix);
+
+    printf("\nMatrix after 90 degree clockwise rotation:\n");
+    printMatrix(n, matrix);
+
+    return 0;
+}
